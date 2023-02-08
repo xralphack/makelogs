3eadf33fab360e4ba49624b112c03cc364413537
xralphack <xralphack@gmail.com>
2023-02-08 22:25:25 +0800

    Add first donkey car article

---
+++ docs/donkey-car/index.md
@@ -0,0 +1,5 @@
+---
+title: Donkey Car
+---
+
+Donkey® 是一個針對遙控車的開源自駕車平台，這個項目包含怎麼組裝一台自己的 Donkey 到後續的軟體開發，車體的部分可以直接用市售的遙控車，搭配 Raspberry Pi 或是 Jetson Nano 與相機，就可以有一個低成本的開發平台，這系列會是我的 Donkey Car 學習記錄
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,62 +1,62 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'makelogs',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "makelogs",
+  tagline: "Dinosaurs are cool",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://makelogs.com',
+  url: "https://makelogs.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   // organizationName: 'facebook', // Usually your GitHub org/user name.
   // projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          routeBasePath: '/',
-          sidebarPath: require.resolve('./sidebars.js'),
+          routeBasePath: "/",
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           // editUrl:
-            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-BB39W1K0D2',
+          trackingID: "G-BB39W1K0D2",
           anonymizeIP: true,
         },
       }),
@@ -67,36 +67,23 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/docusaurus-social-card.jpg",
       navbar: {
-        title: 'makelogs',
+        title: "makelogs",
         logo: {
-          alt: 'makelogs site logo',
-          src: 'img/logo.svg',
+          alt: "makelogs site logo",
+          src: "img/logo.svg",
         },
         items: [
           {
-            to: '/data-structure/intro',
-            position: 'left',
-            label: '資料結構',
-            activeBaseRegex: `/data-structure/`,
+            to: "/donkey-car",
+            position: "left",
+            label: "Donkey Car",
           },
           {
-            to: '/algorithm/intro',
-            position: 'left',
-            label: '演算法',
-            activeBaseRegex: '/algorithm/',
-          },
-          {
-            to: '/pcb/intro',
-            position: 'left',
-            label: 'PCB',
-            activeBaseRegex: '/pcb/',
-          },
-          {
-            to: 'blog',
-            position: 'left',
-            label: 'Blog',
+            to: "blog",
+            position: "left",
+            label: "Blog",
           },
           // {
           //   href: 'https://github.com/facebook/docusaurus',
@@ -106,7 +93,7 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           // {
           //   title: 'Docs',
@@ -135,11 +122,11 @@ const config = {
           //   ],
           // },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Blog",
+                to: "/blog",
               },
               // {
               //   label: 'GitHub',
@@ -154,7 +141,19 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      zoom: {
+        selector: ".markdown :not(em) > img",
+        config: {
+          // options you can specify via https://github.com/francoischalifour/medium-zoom#usage
+          background: {
+            light: "rgb(255, 255, 255)",
+            dark: "rgb(50, 50, 50)",
+          },
+        },
+      },
     }),
+
+  plugins: [require.resolve("docusaurus-plugin-image-zoom")],
 };
 
 module.exports = config;
